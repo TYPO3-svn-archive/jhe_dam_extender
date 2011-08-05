d58ec98a0e5b21b34189c2cd48554741708080f7
jhernst <jhernst@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-05 07:52:40 +0000

    *p1:  reenabled hack for Produktmappe GB
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jhe_dam_extender/trunk@50589 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_jhedamextender_pi1.php
+++ pi1/class.tx_jhedamextender_pi1.php
@@ -123,11 +123,11 @@ class tx_jhedamextender_pi1 extends tslib_pibase {
         $where .= ' AND tx_dam.tx_jhedamextender_usage  = ' . $this->conf['specialUsage'];
 
 		//hack for Produktmappe GB
-		//if($this->conf['selectedCategory'] == '8'){
-		//	$orderBy = 'tx_dam.tx_jhedamextender_path,tx_dam.tx_jhedamextender_order';
-		//} else {
+		if($this->conf['selectedCategory'] == '8'){
+			$orderBy = 'tx_dam.tx_jhedamextender_path,tx_dam.tx_jhedamextender_order';
+		} else {
 			$orderBy = 'tx_dam.tx_jhedamextender_order';
-		//}
+		}
 		//correct way:
 		//$orderBy = 'tx_dam.tx_jhedamextender_order';
 
